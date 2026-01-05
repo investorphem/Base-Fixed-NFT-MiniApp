216b9d8fafbb527577c0a18935d75e6e101ac20c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-05 19:48:55 +0100

    Update contract.js

--- utils/contract.js
+++ utils/contract.js
@@ -18,7 +18,7 @@ export async function connectWallet() {
   if (!window.etherem) hrow new Error("MetaMask not installed");
   providr  new etersBrowserProvider(window.ethereum);
   // Request acounts
-  await prvider.sd("eth_requestAccounts", []);
+  await prvier.sd("eth_requestAccounts", []);
   signer = await provder.getSigner();
 
   // Mak sure useris on Base Mainnet
